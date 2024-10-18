d6447cf14e7d85ab2f0cf3600342060a012fcd2b
emiflow <enzogonzalez104@gmail.com>
2024-10-18 19:07:25 -0300

    Ejemplo events

---
+++ events-ejemplo/index.mjs
@@ -0,0 +1,7 @@
+import {EventEmitter} from 'events';
+const emisor = new EventEmitter();
+emisor.on('saludo',(nombre)=>{
+    console.log(`¡Hola, ${nombre}!`);
+});
+
+emisor.emit('saludo','Mundo');
