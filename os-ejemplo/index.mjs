5df25395b63d29cb1e2837de417be7708f337a36
emiflow <enzogonzalez104@gmail.com>
2024-10-18 18:38:23 -0300

    Ejemplo os

---
+++ os-ejemplo/index.mjs
@@ -0,0 +1,6 @@
+import os from 'os';
+console.log('Arquitectura:',os.arch());
+console.log('Plataforma:',os.platform());
+console.log('Memoria total:',os.totalmem());
+console.log('Memoria libre:',os.freemem());
+console.log('Informacion de la CPU:',os.cpus());
